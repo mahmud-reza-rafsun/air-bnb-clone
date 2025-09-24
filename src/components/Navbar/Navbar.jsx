6cdef9404bf5fe439ac9f77c668166025e73aa89
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2025-09-24 13:23:51 +0600

    inital navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,55 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+import logo from '../../assets/images/logo.png'
+import { FaBars } from "react-icons/fa6";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { TfiWorld } from "react-icons/tfi";
+
 
 const Navbar = () => {
     return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm">
+        <div className="bg-base-100 shadow-sm">
+            <div className="navbar container mx-auto">
                 <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link>
+                        <img src={logo} alt="" />
+                    </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
                         <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
+                            <Link to="/">Home</Link>
                         </li>
-                        <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+                <div className="navbar-end gap-x-3">
+                    <div>
+                        <button className="btn border-none shadow-none rounded-4xl bg-transparent hover:bg-gray-200 duration-500">Become a Host</button>
+                    </div>
+                    <div className="btn border-none shadow-none py-2 px-3 bg-gray-200 rounded-full cursor-pointer">
+                        <TfiWorld className="text-lg" />
+                    </div>
+                    <div className="dropdown p-0">
+                        <div tabIndex={0} role="button" className="btn border-none shadow-none p-3 bg-gray-200 rounded-full cursor-pointer">
+                            <FaBars className="text-base" />
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 absolute right-0 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li className="mb-1">
+                                <Link className="text-[14px]"><FaRegQuestionCircle className="text-base" /> <span >Help Center</span></Link>
+                            </li>
+                            <li className="py-2 border-y border-gray-300">
+                                <span className="text-[14px] font-semibold">Become A Host</span>
+                                <p>It's easy to start hosting and earn extra income.</p>
+                            </li>
+                            <div className="py-2 border-b border-gray-300">
+                                <li><Link className="text-[14px]">Refer a Host</Link></li>
+                                <li><Link className="text-[14px]">Refer a co-Host</Link></li>
+                                <li><Link className="text-[14px]">Gift cards</Link></li>
+                            </div>
+                            <li className="mt-1"><Link className="text-[14px]">Login in or Sign Up</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
