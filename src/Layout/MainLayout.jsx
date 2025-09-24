39ee9ce359c51e62aa5eb6cad10f5b3bb23af9cc
Mahmud Reza Rafsun <97410422+mahmud-reza-rafsun@users.noreply.github.com>
2025-09-24 12:39:28 +0600

    project setup done

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="min-h-[calc(100vh-425px)]">
+                <Outlet/>
+            </div>
+            <Footer/>
+        </div>
+    );
+};
+
+export default MainLayout;
